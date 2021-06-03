1ce0df3c5423be839b9590a5f4e11e3059955b49
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-03 18:06:31 -0400

    Timers added

--- src/components/apps/timer-apps/TimePiecesApp.js
+++ src/components/apps/timer-apps/TimePiecesApp.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import TimerApp from './TimerApp';
-// import ClockApp from './ClockApp';
-// import StopWatchApp from './StopWatchApp'
+import ClockApp from './ClockApp';
+import StopWatchApp from './StopWatchApp'
 
 const TimePiecesApp = () => {
+    
     return(
         <div className='main'>
             <div className='mainDiv'>
                 <TimerApp />
                 <hr />
-                {/* <ClockApp />
+                <ClockApp />
                 <hr />
-                <StopWatchApp /> */}
+                <StopWatchApp />
 
             </div>
         </div>
